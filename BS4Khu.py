b9cc121f59d4642f428d0a434720e328ac962f33
iampeymankhalili <iampeymankhalili@gmail.com>
2023-01-07 03:28:21 +0330

    The second step of the beautiful soup project; Import the beautiful soup library and find a specific ID in the html code of the desired website.

--- BS4Khu.py
+++ BS4Khu.py
@@ -1,10 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 
 URL = "https://iampeymankhalili.github.io/Html4BS/"
 page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
 
+results = soup.find(id="content-section")
+
+print(results.prettify())
 print(page.text)
 
 
