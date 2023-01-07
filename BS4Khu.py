9eee97459828fdd4938c6e3fa94da3991a8859f5
iampeymankhalili <iampeymankhalili@gmail.com>
2023-01-07 04:13:53 +0330

    The third step of this project is to find the data by specifying the class name and then return the text from it.

--- BS4Khu.py
+++ BS4Khu.py
@@ -1,15 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
+# add URL and Get data from Website - step1
 URL = "https://iampeymankhalili.github.io/Html4BS/"
 page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-
-results = soup.find(id="content-section")
-
-print(results.prettify())
 print(page.text)
+# step1
 
+# make a object from BeautifulSoup - step2
+soup = BeautifulSoup(page.content, "html.parser")
+# step2
+
+# Find data with a specific ID - step3
+result_id = soup.find(id="content-section")
+print(result_id.prettify())
+# step3
+
+# Find data with a specific Class name - step4
+result_classes = soup.find_all("div", class_="col-12 col-md-auto")
+for result_class in result_classes:
+    print(result_class, end="\n"*2)
+# step4
+
+#Find text of each class - step5
+for result_class in result_classes:
+    temp = result_class.find("a")
+    print(temp.text)
+#step5
 
